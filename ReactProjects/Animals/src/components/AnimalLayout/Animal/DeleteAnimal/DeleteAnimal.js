a8976eb37f4d77b557da16e6ac5472aaebbd0361
Ambarish-at-507475060344 <ambarish.roy.tech@gmail.com>
2024-04-06 09:18:58 +0530

    Updated folder structure

--- ReactProjects/Animals/src/components/AnimalLayout/Animal/DeleteAnimal/DeleteAnimal.js
+++ ReactProjects/Animals/src/components/AnimalLayout/Animal/DeleteAnimal/DeleteAnimal.js
@@ -1,22 +1,23 @@
-import React from "react";
+import React from 'react';
 import DeleteImage from '../../../../images/delete.png';
 
 const DeleteAnimal = (props) => {
+	const onDeleteClicked = (event) => {
+		props.onDeleteAnimal(props.animal);
+	};
 
-  const onDeleteClicked = (event) => {
-    props.onDeleteAnimal(props.animal);
-  }
-
-    return (
-        <figure class="image is-48x48">
-          <a href="#" onClick={onDeleteClicked}>
-            <img
-              src={DeleteImage}
-              alt="Delete"
-            />
-          </a>
-        </figure>
-    );
-}
+	return (
+		<figure className='image is-48x48'>
+			<a
+				href='#'
+				onClick={onDeleteClicked}>
+				<img
+					src={DeleteImage}
+					alt='Delete'
+				/>
+			</a>
+		</figure>
+	);
+};
 
 export default DeleteAnimal;
--- ReactProjects/Animals/src/components/AnimalLayout/Animal/Like/Like.js
+++ ReactProjects/Animals/src/components/AnimalLayout/Animal/Like/Like.js
@@ -1,26 +1,34 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from 'react';
 import LikeImage from '../../../../images/like.jpeg';
-import "./Like.css";
+import './Like.css';
 
 const Like = () => {
-  
-  const [likeCounter, setLikeCounter] = useState(0);
+	const [likeCounter, setLikeCounter] = useState(0);
 
-  const handleLikeCounterClick = () => {
-    setLikeCounter(likeCounter + 1);
-  }
+	const handleLikeCounterClick = () => {
+		setLikeCounter(likeCounter + 1);
+	};
 
-    return (
-      <a href="#" onClick={handleLikeCounterClick}>
-        <figure class="image is-48x48">
-          <img
-            src={LikeImage}
-            alt="Like"
-          />
-        </figure>
-        <span>{likeCounter}</span>
-      </a>
-    );
-}
+	useEffect(() => {
+		// Don't do this!
+		document.body.onClick = () => {
+			console.log(likeCounter);
+		};
+	}, []);
+
+	return (
+		<a
+			href='#'
+			onClick={handleLikeCounterClick}>
+			<figure className='image is-48x48'>
+				<img
+					src={LikeImage}
+					alt='Like'
+				/>
+			</figure>
+			<span>{likeCounter}</span>
+		</a>
+	);
+};
 
 export default Like;
